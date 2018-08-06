ddebc8d060e00b80357ac3d3802e6e83957224fa
leonhano <haohan819@gmail.com>
2018-08-06 00:52:07 -0400

    - add spawn chests - add smart contract

---
+++ Assets/ImportResources/Chests/TreasureChestMaker/TCM/Scripts/ActivateChest.cs
@@ -0,0 +1,176 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateChest : MonoBehaviour {
+
+	public Transform lid, lidOpen, lidClose;	// Lid, Lid open rotation, Lid close rotation
+	public float openSpeed = 5F;				// Opening speed
+	public bool canClose;						// Can the chest be closed
+
+
+    public const float raiseSpeed = 5f;             //raise speed
+    const float raiseTimerDuration = 1f;
+    
+    private Color[] colors;
+    private bool logInitialFadeSequence = false;
+
+    [HideInInspector]
+	public bool _open;                          // Is the chest opened
+    public bool _raise = false;                          // Is the chest raised
+    public float raiseTimer;
+    public bool fadeout = false;
+
+    private void Start()
+    {
+    }
+
+    void Update () {
+		if(_open){
+			ChestClicked(lidOpen.rotation);
+		}
+		else{
+			ChestClicked(lidClose.rotation);
+		}
+
+        if (_raise)
+        {
+            gameObject.transform.transform.Translate(Vector3.forward * raiseSpeed * Time.deltaTime);
+            raiseTimer += Time.deltaTime;
+        }
+
+        if(raiseTimer >= raiseTimerDuration)
+        {
+            _raise = false;
+            fadeout = true;
+            this.gameObject.SetActive(false);
+        }
+    }
+    
+
+    // Rotate the lid to the requested rotation
+    void ChestClicked(Quaternion toRot){
+		if(lid.rotation != toRot){
+			lid.rotation = Quaternion.Lerp(lid.rotation, toRot, Time.deltaTime * openSpeed);
+		}
+	}
+
+    void doChestAnimation()
+    {
+        _raise = true;
+        raiseTimer = 0f;
+
+        FadeOut(raiseTimerDuration);
+        openOrClose();
+
+    }
+
+    void openOrClose()
+    {
+        if (canClose) _open = !_open; else _open = true;
+    }
+	
+	void OnMouseDown(){
+        //doChestAnimation();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject colliderGO = other.gameObject;
+        if (colliderGO.tag == TagDef.Player)
+            doChestAnimation();        
+    }
+
+    // fade sequence
+    IEnumerator FadeSequence(float fadingOutTime)
+    {
+        // log fading direction, then precalculate fading speed as a multiplier
+        bool fadingOut = (fadingOutTime < 0.0f);
+        float fadingOutSpeed = 1.0f / fadingOutTime;
+
+        // grab all child objects
+        Renderer[] rendererObjects = GetComponentsInChildren<Renderer>();
+        if (colors == null)
+        {
+            //create a cache of colors if necessary
+            colors = new Color[rendererObjects.Length];
+
+            // store the original colours for all child objects
+            for (int i = 0; i < rendererObjects.Length; i++)
+            {
+                colors[i] = rendererObjects[i].material.color;
+            }
+        }
+
+        // make all objects visible
+        for (int i = 0; i < rendererObjects.Length; i++)
+        {
+            rendererObjects[i].enabled = true;
+        }
+
+
+        // get current max alpha
+        float alphaValue = MaxAlpha();
+
+
+        // This is a special case for objects that are set to fade in on start. 
+        // it will treat them as alpha 0, despite them not being so. 
+        if (logInitialFadeSequence && !fadingOut)
+        {
+            alphaValue = 0.0f;
+            logInitialFadeSequence = false;
+        }
+
+        // iterate to change alpha value 
+        while ((alphaValue >= 0.0f && fadingOut) || (alphaValue <= 1.0f && !fadingOut))
+        {
+            alphaValue += Time.deltaTime * fadingOutSpeed;
+
+            for (int i = 0; i < rendererObjects.Length; i++)
+            {
+                Color newColor = (colors != null ? colors[i] : rendererObjects[i].material.color);
+                newColor.a = Mathf.Min(newColor.a, alphaValue);
+                newColor.a = Mathf.Clamp(newColor.a, 0.0f, 1.0f);
+                rendererObjects[i].material.SetColor("_Color", newColor);
+            }
+
+            yield return null;
+        }
+
+        // turn objects off after fading out
+        if (fadingOut)
+        {
+            for (int i = 0; i < rendererObjects.Length; i++)
+            {
+                rendererObjects[i].enabled = false;
+            }
+        }
+
+
+        Debug.Log("fade sequence end : " + fadingOut);
+
+    }
+
+    void FadeIn(float newFadeTime)
+    {
+        StopAllCoroutines();
+        StartCoroutine("FadeSequence", newFadeTime);
+    }
+
+    void FadeOut(float newFadeTime)
+    {
+        StopAllCoroutines();
+        StartCoroutine("FadeSequence", -newFadeTime);
+    }
+
+    // check the alpha value of most opaque object
+    float MaxAlpha()
+    {
+        float maxAlpha = 0.0f;
+        Renderer[] rendererObjects = GetComponentsInChildren<Renderer>();
+        foreach (Renderer item in rendererObjects)
+        {
+            maxAlpha = Mathf.Max(maxAlpha, item.material.color.a);
+        }
+        return maxAlpha;
+    }
+}
